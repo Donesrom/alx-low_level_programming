44468c652ab4910fd21bc76ae35d81fa9fa38771
Donesrom <donesrom@gmail.com>
2022-03-10 12:51:15 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-
 /**
- *main - prints the size of various types
- *Return - 0
+ * main - prints the size of various types
+ *Description: produce the exact same output
+ * Return: 0 if exited properly, non-zero otherwise
  */
+
 
 int main(void)
 {
