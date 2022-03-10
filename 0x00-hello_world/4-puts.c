a2cd7fda46aaecc3961d54df3b63411d9a0564c8
Donesrom <donesrom@gmail.com>
2022-03-10 11:02:01 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-/* prints "Programming is like building a multilingual puzzle, followed by a new line */
+/**
+
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+
+ * Return: 0 if exited properly, non-zero otherwise
+
+ */
+
 
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle")
         return(0)
 }
-    
+ 
---
+++ 0x00-hello_world/4-puts.c~
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* prints "Programming is like building a multilingual puzzle, followed by a new line */
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle")
+        return(0)
+}
+    
