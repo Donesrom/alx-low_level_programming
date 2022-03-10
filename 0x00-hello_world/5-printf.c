fb8dd5c6e37dca89ab1dd218db65db9bd233c5a4
Donesrom <donesrom@gmail.com>
2022-03-10 11:55:06 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,13 @@
 
 
 /**
- *main:prints exactly with proper grammar,
+ *main - prints exactly with proper grammar,
  * but the outcome is a piece of art
- *return :0
+ *Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return(0)
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
