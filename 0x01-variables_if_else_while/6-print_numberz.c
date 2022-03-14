ade38f3ad7100bf5022863be3fc9ce670ded7821
Donesrom <donesrom@gmail.com>
2022-03-13 20:04:31 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stio.h>
+
+/**
+ *main-  a program that prints all single digit numbers
+ *of base 10 starting from 0
+ *Return: Alwasy 0 (success)
+ */
+
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(n);
+}
+putchar(\n');
+
+return (0);
+}
