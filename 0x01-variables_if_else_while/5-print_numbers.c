b03d77d685f63817d28f90ccd95c9441be99ca6a
Donesrom <donesrom@gmail.com>
2022-03-13 19:49:12 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib>
+#include <stdio>
+
+/**
+ *main - a program that prints all single digit numbers
+ *of base 10 starting from 0
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+for (n = 0; n <= 10; n++)
+printf("5d", n);
+printf("\n")
+
+return (0);
+}
