88c25f876fd02722883d444e56272e8ea7330fdc
Donesrom <donesrm@gmail.com>
2022-03-15 13:15:14 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main -  function that prints the alphabet
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+
+int main(void);
+
+{
+print_alphabet();
+
+return (0);
+
+}
