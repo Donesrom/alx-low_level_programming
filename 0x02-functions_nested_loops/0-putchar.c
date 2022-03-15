e2786d8f954767fc4c0c64992d0ccdd1eed2f6f9
Donesrom <donesrm@gmail.com>
2022-03-15 12:18:30 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main a program that prints _putchar,
+*followed by a new line
+* Return: Always O (Success)
+*/
+
+int main(void)
+{
+char = " _putchar";
+printf(char);
+
+return (0);
+}
