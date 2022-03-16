d5e5e4d5cc26f331610006c0a1eadd4902a12af3
Donesrom <donesrm@gmail.com>
2022-03-15 20:41:55 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  *Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
 
-int _isalpha(int c);
+int _isalpha(int c)
 {
 return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
 }
