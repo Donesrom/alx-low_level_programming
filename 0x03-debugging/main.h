84afb77e070500abf846762b0b7f87013ec2fa6e
Donesrom <donesrm@gmail.com>
2022-03-16 23:51:39 -0700

    New

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,15 @@
-#include "main.h"
+#ifndef MAIN_H
 
-/**
- *main - tests function that prints if integer is positive or negative
- *Return: 0
- */
-
-int main(void)
-{
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
-}
+#define MAIN_H
 
 
+
+#include <stdio.h>
+
+
+
+void positive_or_negative(int i);
+
+
+
+#endif /* MAIN_H */
