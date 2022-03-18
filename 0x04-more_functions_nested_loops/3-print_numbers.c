07d18262471bf312a1c0f5744a7ac47cda3bd580
Donesrom <donesrm@gmail.com>
2022-03-17 18:48:05 -0700

    _putchar fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "_putchar.c"
 
 /**
  * print_numbers - function that prints the numbers, from 0 to 9
@@ -8,11 +9,11 @@
 void print_numbers(void)
 {
 int i;
+
 for (i = 0; i < 10; i++)
 {
-_putchar(i + 0);
+_putchar(i);
 }
 _putchar('\n');
 }
-return (0);
-}
+
