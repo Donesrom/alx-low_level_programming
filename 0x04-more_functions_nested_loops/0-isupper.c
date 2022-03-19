cbcd515dccefc95f934739c9ef9e6db155a6901b
Donesrom <donesrm@gmail.com>
2022-03-18 18:37:42 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper- function that checks for uppercase character
+ *_isupper- checks for uppercase character
  * @c: the character to be checked
- * Returns 1 if c is uppercase 0 otherwise
+ * Return: 1 if c is uppercase 0 otherwise
  */
 
 int _isupper(int c)
