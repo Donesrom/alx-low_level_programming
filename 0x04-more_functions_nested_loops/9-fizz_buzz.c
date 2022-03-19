352fa2d067719548aeabaaf28b759673c94266b4
Donesrom <donesrm@gmail.com>
2022-03-18 18:29:33 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,5 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
-
 
 
 /**
@@ -11,24 +10,28 @@
  */
 
 int main(void)
-
 {
+
 int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
 printf(" Fizz");
-} else if (i % 5 == 0 && i % 3 != 0)
+}
+else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
-} else if (i % 3 == 0 && i % 5 == 0)
+}
+else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
-} else if (i == 1)
+}
+else if (i == 1)
 {
 printf("%d", i);
-} else
+}
+else
 {
 printf(" %d", i);
 }
@@ -36,4 +39,3 @@ printf(" %d", i);
 printf("\n");
 return (0);
 }
-
