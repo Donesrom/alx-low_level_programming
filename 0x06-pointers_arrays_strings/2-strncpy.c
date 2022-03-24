b8bf5e1868a2a20a541efb6a125fa2988c311ba6
Donesrom <donesrm@gmail.com>
2022-03-24 05:42:29 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * _strncat-function that concatenates two strings.
+ * @dest:destination string
+ * @src:source string
+ * @n:number of bytes to concancate
+ * Return:a pointer to the resulting string dest
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+
+i = 0;
+
+while (src[i] != '\0' && i < n)
+{
+
+dest[i] = src[i];
+i++;
+
+}
+
+while (i < n)
+{
+
+dest[i] != '\0';
+i++;
+}
+return (dest);
+}
