76b942601dba2146a08f5bc5eab05293d9bc9653
Donesrom <donesrm@gmail.com>
2022-03-25 08:15:36 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,15 +2,14 @@
 
 
 /**
- * _strncat-function that copies a string.
+ * _strncpy-function that copies a string.
  * @dest:destination string
  * @src:source string
  * @n:number of bytes to copy
  * Return:a pointer to the resulting string dest
  */
 
-
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 
