f6ff765f1b68847d67271add2e281a580535bd68
Donesrom <donesrm@gmail.com>
2022-04-11 18:48:31 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * strut dog- basic info
+ * @name: First member
+ * @age: second member
+ * @owner: Third member
+ * Description: Longer description
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
