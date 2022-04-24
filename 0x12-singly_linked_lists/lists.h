a9923c984a5f02eab9121706429d6884fcb63aeb
Donesrom <donesrom@gmail.com>
2022-04-24 06:03:01 -0700

    changes to 0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,17 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ * Description: singly linked list node structur
+ * for Holberton project
+ */
+
+
 typedef struct list_s
 
 {
